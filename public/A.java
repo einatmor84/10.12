26d32e36412dcae02f700fb4b2debbe6b5e91d60
einatmor84 <144316208+einatmor84@users.noreply.github.com>
2023-12-10 20:51:04 +0200

    Add files via upload

---
+++ public/A.java
@@ -0,0 +1,12 @@
+
+public class A {
+    public A(int x) {
+        this.x = x;
+    }
+    public int x ;
+    public void classAMethod(){
+        System.out.println("hello from class A");
+        System.out.println(x);
+    }
+
+}
---
+++ public/Second/C.java
@@ -0,0 +1,7 @@
+package Second;
+public class C {
+    public int x = 20;
+    public void classCMethod(){
+        System.out.println("hello from class C package Second");
+    }
+}
