09683a9131c11224751baeeb55fe2ccee3afa023
einatmor84 <144316208+einatmor84@users.noreply.github.com>
2023-12-10 20:48:03 +0200

    Add files via upload

---
+++ Defualt/A.java
@@ -0,0 +1,12 @@
+
+public class A {
+     A(int x) {
+        this.x = x;
+    }
+     int x ;
+     void classAMethod(){
+        System.out.println("hello from class A");
+        System.out.println(x);
+    }
+
+}
---
+++ Defualt/Second/C.java
@@ -0,0 +1,7 @@
+package Second;
+public class C {
+     int x = 20;
+     void classCMethod(){
+        System.out.println("hello from class C package Second");
+    }
+}
